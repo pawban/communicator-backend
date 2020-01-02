b21aad4b5c20e3d9384cef913806de94df183cd5
gawelx <gawelx@gmail.com>
2020-01-02 13:00:15 +0100

    Final commit - config files and README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# Communicator REST API
+
+### Short description
+This is application providing REST API for anonymous internet communicator.
+
+### Instructions
+To run this app you have to provide PostgreSQL database to store data.
+Database url, username and login should be defined in system properties on server running this app. 
+
+### Frontend
+[Git hub repository](https://github.com/pawban/communicator-frontend)
--- src/main/java/com/pawban/communicator/config/CoreConfiguration.java
+++ src/main/java/com/pawban/communicator/config/CoreConfiguration.java
@@ -1,15 +1,37 @@
 package com.pawban.communicator.config;
 
+import com.pawban.communicator.converter.StringToAccessRequestRoleConverter;
+import com.pawban.communicator.converter.StringToAccessRequestStatusConverter;
+import com.pawban.communicator.converter.StringToChatRoomStatusConverter;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+@EnableScheduling
 @Configuration
-public class CoreConfiguration {
+@Getter
+public class CoreConfiguration implements WebMvcConfigurer {
+
+    @Value("${communicator.app.restricted.usernames}")
+    private List<String> restrictedUserNames;
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToChatRoomStatusConverter());
+        registry.addConverter(new StringToAccessRequestRoleConverter());
+        registry.addConverter(new StringToAccessRequestStatusConverter());
     }
 
 }
