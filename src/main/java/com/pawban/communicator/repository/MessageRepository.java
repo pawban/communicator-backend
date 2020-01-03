0ef88eb72faea3c530f83787c94c5acf8ead5b98
gawelx <gawelx@gmail.com>
2020-01-03 18:14:42 +0100

    Unused method from MessageRepository removed

--- src/main/java/com/pawban/communicator/repository/MessageRepository.java
+++ src/main/java/com/pawban/communicator/repository/MessageRepository.java
@@ -5,13 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
-import java.util.List;
 import java.util.UUID;
 
 @Transactional
 @Repository
 public interface MessageRepository extends CrudRepository<Message, UUID> {
-
-    List<Message> findAllByChatRoomId(UUID chatRoomId);
 
 }
